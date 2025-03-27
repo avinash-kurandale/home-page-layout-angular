2df61cf1ae13843910348f6aa30b1d5295d62e2c
avinash-kurandale <avinashkurandale002@gmail.com>
2025-03-28 01:11:03 +0530

    layout of home page

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { MainComponent } from "./components/main/main.component";
+import { FooterComponent } from "./components/footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, MainComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-layout';
+}
